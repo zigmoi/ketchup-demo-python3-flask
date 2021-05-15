171d0fcb8c1add3ee9acfeb5a6c49e8357226884
ashimusmani <ashimusmani@gmail.com>
2021-05-15 20:58:37 +0530

    Added gunicorn web server support and log statement.

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    print("Received request.")
     return 'Hello World!'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9888)
 
