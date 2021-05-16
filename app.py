301eafeae5243e74099fed876dc46adc8c84a05c
ashimusmani <ashimusmani@gmail.com>
2021-05-16 14:29:18 +0530

    Updated print statements to info logs.

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from flask import Flask
+import logging
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.INFO)
 
 @app.route('/')
 def hello():
-    print("Received request.")
+    app.logger.info("Request Received.")
     return 'Hello World!'
 
